95f9187d7a76c460c42005d860010e8c2ffb0806
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-09-16 18:33:21 +0000

    C++ math

---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+// Created by: Yoma Ozoh
+// Created on: September 5, 2025
+// This program does basic math.
+// Copyright 2025 Yoma
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "22 + 4 = " << 22 + 4 << std::endl;
+    std::cout << "8 - 5 = " << 8 - 5 <<std::endl;
+    std::cout << "121 / 11 = " << 121 / 11 << std::endl;
+    std::cout << "12 - 6 / 3 = " << 12 - 6 / 3 << std::endl;
+    std::cout << "5^8 = " << pow(5, 8) << std::endl;
+    std::cout << "17 /  = " << float(1) / float(3) << std::endl;
+}
